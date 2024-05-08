3f16edc13bffb15a6f46be67f85eb44a5f764a79
gkelvin6 <130702660+gkelvin6@users.noreply.github.com>
2024-05-09 03:11:42 +0800

    Update apiRequest.js

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL: "https://kg888-back.onrender.com/api",
-  withCredentials: true,
+  withCredentials: true,httpOnly: false,
+      sameSite: 'none'
 });
 
 export default apiRequest;
